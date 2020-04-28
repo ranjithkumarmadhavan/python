deeab33432a9c29b133e7a3446d216c985fa1910
Ranjith Kumar <ranjithkumarmadhavan@gmail.com>
2020-04-28 23:21:29 +0530

    mod

--- Projects/app/whatsapp automation/whatsapp automation.py
+++ Projects/app/whatsapp automation/whatsapp automation.py
@@ -18,6 +18,7 @@ user.click()
 msg_box = driver.find_element_by_class_name('_3F6QL._2WovP') #_3F6QL._2WovP
 
 for i in range(count):
+    print(i)
     msg_box.send_keys(msg)
     button = driver.find_element_by_class_name('_35EW6')
     button.click()
